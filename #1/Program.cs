d1f03b8d8f6c101960fb0b7fbbfe4340a2729478
Константин <konstkonst55@mail.ru>
2026-09-10 08:22:52 +0700

    feat: add main sort program

--- #1/Program.cs
+++ #1/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace _1
+using _1.Algorithms;
+
+var random = new Random();
+var source = new int[10];
+
+for (var i = 0; i < source.Length; i++)
 {
-    internal class Program
+    source[i] = random.Next(1, 101);
+}
+
+Console.WriteLine($"Source: {string.Join(", ", source)}");
+Console.WriteLine();
+
+var algorithms = new ISortingAlgorithm[]
+{
+    new BubbleSort(),
+    new SelectionSort(),
+    new MergeSort()
+};
+
+foreach (var algorithm in algorithms)
+{
+    var result = algorithm.Sort(source);
+
+    Console.WriteLine(algorithm.Name);
+
+    foreach (var step in result.Steps)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine(step);
     }
+
+    Console.WriteLine($"Sorted: {string.Join(", ", result.Values)}");
+    Console.WriteLine($"m = {result.Movements}, c = {result.Comparisons}");
+    Console.WriteLine();
 }
